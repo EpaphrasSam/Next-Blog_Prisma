d0e7ab5fa218dd78326cc1383a2e1b1cfb648980
EpaphrasSam <isinesam@gmail.com>
2023-10-13 01:22:58 +0000

    Update axios.js

--- utils/axios.js
+++ utils/axios.js
@@ -1,6 +1,9 @@
 const axios = require("axios").default;
 
-const url = "http://localhost:3000/";
+const url =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/"
+    : "https://blogosphere-ng6fmu5oa-epaphrassam.vercel.app/";
 
 export default axios.create({
   baseURL: url,
